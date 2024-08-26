d57e8657ec7f512ba73321513759f414f7278c11
AgusCorren <am.correnti@alumno.um.edu.ar>
2024-08-26 13:00:40 -0300

    Terminando de refactorizar pawn

--- game/pieces/pawn.py
+++ game/pieces/pawn.py
@@ -9,48 +9,34 @@ class Pawn(Piece):
         return "♙" if self.__color__ == "white" else "♟"
     
     def check_move(self, positions, new_position):
-
-
         x, y, current_x, current_y = self.get_coordinates(new_position)
 
         if self.__color__ == "white":
             return self.is_valid_white_move(x, y, current_x, current_y, positions)
         elif self.__color__ == "black":
             return self.is_valid_black_move(x, y, current_x, current_y, positions)
-
         return False
-    
+
     def is_valid_white_move(self, x, y, current_x, current_y, positions):
+        ## Valida si el tipo de movimiento del peon blanco, envia la direccion y la fila inicial
+        return self.is_valid_pawn_move(x, y, current_x, current_y, positions, -1, 6)
 
-        result = False
-
-        if y == current_y:
-                if current_x == 6:
-                    if x == current_x - 1 and positions[current_x - 1][current_y] is None:
-                        result = True
-                    elif x == current_x - 2 and positions[current_x - 1][current_y] is None and positions[current_x - 2][current_y] is None:
-                        result = True
-                elif x == current_x - 1 and positions[current_x - 1][current_y] is None:
-                    result = True
-            # Movimiento de captura
-        elif x == current_x - 1 and abs(y - current_y) == 1 and positions[x][y] is not None:
-            result = True
-
-        return result
-    
     def is_valid_black_move(self, x, y, current_x, current_y, positions):
+        ## Valida si el tipo de movimiento del peon negro, envia la direccion y la fila inicial
+        return self.is_valid_pawn_move(x, y, current_x, current_y, positions, 1, 1)
 
+    def is_valid_pawn_move(self, x, y, current_x, current_y, positions, direction, initial_row):
         result = False
-
         if y == current_y:
-                if current_x == 1:
-                    if x == current_x + 1 and positions[current_x + 1][current_y] is None:
-                        result = True
-                    elif x == current_x + 2 and positions[current_x + 1][current_y] is None and positions[current_x + 2][current_y] is None:
-                        result = True
-                elif x == current_x + 1 and positions[current_x + 1][current_y] is None:
+        # Valida que el movimiento sea en la misma columna
+            if current_x == initial_row:
+                if x == current_x + direction and positions[current_x + direction][current_y] is None:
                     result = True
+                elif x == current_x + 2 * direction and positions[current_x + direction][current_y] is None and positions[current_x + 2 * direction][current_y] is None:
+                    result = True
+            elif x == current_x + direction and positions[current_x + direction][current_y] is None:
+                result = True
+        elif x == current_x + direction and abs(y - current_y) == 1 and positions[x][y] is not None:
         # Movimiento de captura
-        elif x == current_x + 1 and abs(y - current_y) == 1 and positions[x][y] is not None:
             result = True
         return result
